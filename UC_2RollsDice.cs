c50125aad96687cc8b251664b7095d8d341426ac
AkarshRinku <86792553+AkarshRinku@users.noreply.github.com>
2021-07-09 17:34:45 +0530

    [akarsh] added: Player Rolls The Dice

---
+++ UC_2RollsDice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snakes_And_Ladders
+{
+    class UC2_PlayerRollsDice
+    {
+        public static void snake1()
+        {
+            int position = 0;
+            Console.WriteLine("Player is at position: " + position);
+            Random random = new Random();
+            int diceroll = 0;
+            while (true)
+            {
+                diceroll = random.Next(6);
+                if (diceroll != 0)
+
+             break;
+
+            }
+            Console.WriteLine(diceroll);
+        }
+    }
+}
+
