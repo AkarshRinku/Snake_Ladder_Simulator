7047fe9bbcc1be2e254e44883c760400cd999ad3
AkarshRinku <86792553+AkarshRinku@users.noreply.github.com>
2021-07-09 17:03:14 +0530

    [akarsh] added: Starting position at zero

---
+++ UC_1StartingPositionAtZero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snakes_And_Ladders
+{
+    class UC1_PlayerStartingPositionAtZero
+    {
+        public static void snake()
+        {
+            int position = 0;
+            Console.WriteLine("Player is at position : " + position);
+        }
+    }
+}
+
