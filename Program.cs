ce6fb094572605afdc01338ca653ea9626822a13
AkarshRinku <86792553+AkarshRinku@users.noreply.github.com>
2021-07-09 16:59:15 +0530

    Welcome To Snake And Ladder

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Snakes_And_Ladders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            //UC1_PlayerStartingPositionAtZero.snake();
+            //UC2_PlayerRollsDice.snakeAndLadder1();
+            //UC_3OptionsCheck.snakeAndLadder2();
+            //UC_4WinningPosition100.SnakeAndLadder3();
+            //UC_5ExactWinningPosition100.snakeAndLadder4();
+            //UC_6NumberOfDicePlayed.snakeAndLadder5();
+            //UC_7TwoPlayerWinGame.snakeAndLadder6();
+
+
+
+                Console.ReadKey();
+
+
+        }   
+    }
+}
+
